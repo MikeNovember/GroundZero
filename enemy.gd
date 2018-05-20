da884a7cd568c7426042bf7165eafb61772fc7ef
MikeNovember <niwa.mateusz@gmail.com>
2018-05-20 19:17:00 +0200

    Fixed enemies
    Enemies now appear and can be killed.
    Their behaviour is basic: they move between walls
    with constant speed and bounce back and forth upon
    collision with walls but also with eachother (todo)

--- enemy.gd
+++ enemy.gd
@@ -1,22 +1,37 @@
 extends KinematicBody2D
 
+const WALK_SPEED = 140
 const STATE_WALKING = 0
 const STATE_KILLED = 1
 
+var linear_velocity = Vector2()
+var direction = 1
+var anim=""
+
 var state = STATE_WALKING
 
-# Member variables
-export var motion = Vector2()
-export var cycle = 1.0
-var accum = 0.0
+onready var sprite = $sprite
 
 func _physics_process(delta):
-	accum += delta * (1.0 / cycle) * PI * 2.0
-	accum = fmod(accum, PI * 2.0)
-	var d = sin(accum)
-	var xf = Transform2D()
-	xf[2]= motion * d
-	transform = xf
+	var new_anim = "idle"
+
+	if state==STATE_WALKING:
+		if is_on_wall():
+			direction *= -1.0
+
+		linear_velocity.x = direction * WALK_SPEED
+		linear_velocity = move_and_slide(linear_velocity)
+
+		sprite.scale = Vector2(direction, 1.0)
+		new_anim = "walk"
+	else:
+		new_anim = "explode"
+
+
+	if anim != new_anim:
+		anim = new_anim
+		$anim.play(anim)
+
 
 func hit_by_bullet():
 	state = STATE_KILLED
