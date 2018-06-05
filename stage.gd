1a8e3feab77470ea7846dfa87adb85919243121f
Kamil Baran <QDNB84@motorolasolutions.com>
2018-06-05 21:46:30 +0200

    Basic score display mechanics on HUB

--- stage.gd
+++ stage.gd
@@ -1,5 +1,10 @@
 extends Node
 
+
 func _ready():
+	$player.connect("shoot",$HUD,"on_shoot"); 
+	#this.connect("point_inc",$HUD,"on_point_inc");
+	#this.connect("point_dec",$HUD,"on_point_dec");
 	for coin in $coins.get_children():
 		coin.connect("coin_picked", $player, "on_coin_picked")
+		coin.connect("coin_picked",$HUD,"on_coin_picked")
